b1ea1922c6e9b64330211461a7a656905dc75f2c
Jingxing <1464267067@qq.com>
2018-04-11 16:55:07 +0800

    Add files via upload

---
+++ client/client/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace client
+{
+    partial class client
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.sendMessageTextBox = new System.Windows.Forms.TextBox();
+            this.sendMessageButton = new System.Windows.Forms.Button();
+            this.receiveTextBox = new System.Windows.Forms.TextBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ip";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "127.0.0.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "port";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(59, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(44, 21);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "8888";
+            // 
+            // sendMessageTextBox
+            // 
+            this.sendMessageTextBox.Location = new System.Drawing.Point(14, 123);
+            this.sendMessageTextBox.Multiline = true;
+            this.sendMessageTextBox.Name = "sendMessageTextBox";
+            this.sendMessageTextBox.Size = new System.Drawing.Size(200, 113);
+            this.sendMessageTextBox.TabIndex = 4;
+            // 
+            // sendMessageButton
+            // 
+            this.sendMessageButton.Location = new System.Drawing.Point(137, 242);
+            this.sendMessageButton.Name = "sendMessageButton";
+            this.sendMessageButton.Size = new System.Drawing.Size(75, 23);
+            this.sendMessageButton.TabIndex = 5;
+            this.sendMessageButton.Text = "发送";
+            this.sendMessageButton.UseVisualStyleBackColor = true;
+            this.sendMessageButton.Click += new System.EventHandler(this.sendMessageButton_Click);
+            // 
+            // receiveTextBox
+            // 
+            this.receiveTextBox.Location = new System.Drawing.Point(252, 29);
+            this.receiveTextBox.Multiline = true;
+            this.receiveTextBox.Name = "receiveTextBox";
+            this.receiveTextBox.Size = new System.Drawing.Size(186, 196);
+            this.receiveTextBox.TabIndex = 6;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(363, 242);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 7;
+            this.clearButton.Text = "清空";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(14, 83);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(75, 23);
+            this.connectButton.TabIndex = 9;
+            this.connectButton.Text = "连接服务器";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 297);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.receiveTextBox);
+            this.Controls.Add(this.sendMessageButton);
+            this.Controls.Add(this.sendMessageTextBox);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "client";
+            this.Text = "client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox sendMessageTextBox;
+        private System.Windows.Forms.Button sendMessageButton;
+        private System.Windows.Forms.TextBox receiveTextBox;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button connectButton;
+    }
+}
+
---
+++ client/client/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+namespace client
+{
+    public partial class client : Form
+    {
+        Thread clientThread = null;
+        Socket clientSocket = null;
+        public client()
+        {
+            InitializeComponent();
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            receiveTextBox.Clear();
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipaddress = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ip=new IPEndPoint(ipaddress,8888);
+            try
+            {
+                clientSocket.Connect(ip);
+            }
+            catch (Exception)
+            {
+                receiveTextBox.AppendText("连接失败\n");
+            }
+            clientThread = new Thread(receiver);
+            clientThread.IsBackground = true;
+            clientThread.Start();
+        }
+
+        private void receiver()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] buff = new byte[1024 * 8];
+                    int length = clientSocket.Receive(buff);
+                    if (length > 0)
+                    {
+                        String message = Encoding.UTF8.GetString(buff);
+                        receiveTextBox.AppendText("server:" + message+"\n");
+                    }
+                }
+                catch(Exception e)
+                {
+                    receiveTextBox.AppendText("连接中断\n");
+                    break;
+                }
+            }
+  
+        }
+
+        private void sendMessageButton_Click(object sender, EventArgs e)
+        {
+            String message = sendMessageTextBox.Text+"\n";
+            byte[] message1 = Encoding.UTF8.GetBytes(message);
+            clientSocket.Send(message1);
+
+        }
+
+
+        
+
+    }
+}
