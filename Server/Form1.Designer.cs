839f26d1371f3f5668a6b4feb94f06c062b8d08f
jingxing-zheng <1464267067@qq.com>
2018-04-11 16:33:00 +0800

    提交文件

---
+++ Server/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Server
+{
+    partial class Server
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.clientListBox = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.sendMessageTextBox = new System.Windows.Forms.TextBox();
+            this.receiveTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.sendMessageButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(45, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "127.0.0.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(175, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "port";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(222, 23);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(67, 21);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "8888";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "连接的客户端";
+            // 
+            // clientListBox
+            // 
+            this.clientListBox.FormattingEnabled = true;
+            this.clientListBox.ItemHeight = 12;
+            this.clientListBox.Location = new System.Drawing.Point(26, 72);
+            this.clientListBox.Name = "clientListBox";
+            this.clientListBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.clientListBox.Size = new System.Drawing.Size(265, 100);
+            this.clientListBox.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "发送消息";
+            // 
+            // sendMessageTextBox
+            // 
+            this.sendMessageTextBox.Location = new System.Drawing.Point(28, 220);
+            this.sendMessageTextBox.Multiline = true;
+            this.sendMessageTextBox.Name = "sendMessageTextBox";
+            this.sendMessageTextBox.Size = new System.Drawing.Size(263, 98);
+            this.sendMessageTextBox.TabIndex = 7;
+            // 
+            // receiveTextBox
+            // 
+            this.receiveTextBox.Location = new System.Drawing.Point(313, 72);
+            this.receiveTextBox.Multiline = true;
+            this.receiveTextBox.Name = "receiveTextBox";
+            this.receiveTextBox.Size = new System.Drawing.Size(232, 246);
+            this.receiveTextBox.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(327, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "启动服务器";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sendMessageButton
+            // 
+            this.sendMessageButton.Location = new System.Drawing.Point(216, 324);
+            this.sendMessageButton.Name = "sendMessageButton";
+            this.sendMessageButton.Size = new System.Drawing.Size(75, 23);
+            this.sendMessageButton.TabIndex = 10;
+            this.sendMessageButton.Text = "发送";
+            this.sendMessageButton.UseVisualStyleBackColor = true;
+            this.sendMessageButton.Click += new System.EventHandler(this.sendMessageButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(470, 324);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 11;
+            this.clearButton.Text = "清空";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 360);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.sendMessageButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.receiveTextBox);
+            this.Controls.Add(this.sendMessageTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.clientListBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Server";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox clientListBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox sendMessageTextBox;
+        private System.Windows.Forms.TextBox receiveTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button sendMessageButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Server/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Collections;
+using System.Net;
+using System.Threading;
+
+namespace Server
+{
+    public partial class Server : Form
+    {
+
+        static Socket serverSocket = null;
+        static Dictionary<String, Socket> clientItem = new Dictionary<String, Socket>();
+        public Server()
+        {
+            InitializeComponent();
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipaddress = IPAddress.Parse("127.0.0.1");
+            serverSocket.Bind(new IPEndPoint(ipaddress, 8888));
+            serverSocket.Listen(10);
+            Thread thread = new Thread(ListenClientConnect);
+            thread.IsBackground = true;
+            thread.Start();
+            receiveTextBox.AppendText("服务器启动,等待连接...\n");
+        }
+        private void ListenClientConnect()
+        {
+            Socket clientSocket = null;
+            while (true)
+            {
+                try
+                {
+                    clientSocket = serverSocket.Accept();
+                }
+                catch (Exception e)
+                {
+                    Console.Write(e.Message);
+                    break;
+                }
+                IPAddress clientIP = (clientSocket.RemoteEndPoint as IPEndPoint).Address;
+                int clientPort = (clientSocket.RemoteEndPoint as IPEndPoint).Port;
+                String client = "IP:" + clientIP + " Port:" + clientPort;
+                String message = client + "连接服务器成功\n";
+                byte[] message1 = Encoding.UTF8.GetBytes(message);
+                clientSocket.Send(message1);
+                receiveTextBox.AppendText("与客户端" + client + "连接成功\n");
+                clientItem.Add(client, clientSocket);
+                clientListBox.Items.Add(client);
+                ParameterizedThreadStart p = new ParameterizedThreadStart(receiver);
+                Thread thread = new Thread(p);
+                thread.IsBackground = true;
+                thread.Start(clientSocket);
+            }
+        }
+
+        private void receiver(Object o)
+        {
+            Socket socket = o as Socket;
+            while (true)
+            {
+                byte[] buff = new byte[1024 * 8];
+                try
+                {
+                    int length = socket.Receive(buff);
+                    if (length > 0)
+                    {
+                        String message = Encoding.UTF8.GetString(buff, 0, length);
+                        String client = socket.RemoteEndPoint + ":";
+                        receiveTextBox.AppendText(client + "\n" + message+"\n");
+                    }
+                }
+                catch (Exception e)
+                {
+                    receiveTextBox.AppendText(socket.RemoteEndPoint + "中断\n" );
+                    IPAddress clientIP = (socket.RemoteEndPoint as IPEndPoint).Address;
+                    int clientPort = (socket.RemoteEndPoint as IPEndPoint).Port;
+                    String client = "IP:" + clientIP + " Port:" + clientPort;
+                    for (int i = 0; i < clientListBox.Items.Count; i++)
+                    {
+                        if (client.Equals(clientListBox.Items[i]))
+                        {
+                            clientListBox.Items.RemoveAt(i);
+                        }
+                    }
+                        clientItem.Remove(client);
+                    break;
+                }
+            }
+        }
+
+        private void sendMessageButton_Click(object sender, EventArgs e)
+        {
+            String message = sendMessageTextBox.Text+"\n";
+            byte[] message1 = Encoding.UTF8.GetBytes(message);
+            if (!String.IsNullOrEmpty(clientListBox.Text.Trim()))
+            {
+                for (int i = 0; i < clientListBox.SelectedItems.Count; i++)
+                {
+                    clientItem[clientListBox.SelectedItems[i].ToString()].Send(message1);
+                }
+            }
+            sendMessageTextBox.Clear();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            receiveTextBox.Clear();
+        }
+
+
+    }
+}
